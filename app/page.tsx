6d69c8e8a0ed5fe86ea114dd6f90ee89a49c67d0
biyash-1 <sthaalish39@gmail.com>
2025-10-30 23:14:51 +0545

    app

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Im creating next app</h1>
-             <p>my first para about next js</p>
-    </div>
-  );
+    <>
+     <div>
+      <h1>Welcome to clouvalu</h1>
+     </div>
+    </>
+  )
 }
