79c4cb2399478e033a568fb934b6fb0825ab1e27
biyash-1 <sthaalish39@gmail.com>
2025-11-10 23:53:19 +0545

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '50mb', // Set your desired size here
+    },
+  },
+  images: {
+    domains: ['fra.cloud.appwrite.io','cdn-icons-png.flaticon.com'], // ✅ add your Appwrite host here
+  },
   /* config options here */
 };
 
