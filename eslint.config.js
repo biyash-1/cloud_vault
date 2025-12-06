a9ef30cfc5ccc285d352946b727ae5a931a22619
biyash-1 <sthaalish39@gmail.com>
2025-12-06 12:44:19 +0545

    type fix of eslins

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,3 @@
-// eslint.config.js
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -6,11 +5,15 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({ baseDirectory: __dirname });
+const compat = new FlatCompat({ 
+  baseDirectory: __dirname,
+  recommendedConfig: {},
+  allConfig: {}
+});
 
 export default [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"],
   },
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
