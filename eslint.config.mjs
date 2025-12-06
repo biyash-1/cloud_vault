e3cfaa5d5a007f80674b601393cd4043038ffacf
biyash-1 <sthaalish39@gmail.com>
2025-12-06 17:45:59 +0545

    type fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,18 @@
-export default [
-  {
-    files: ["**/*.{js,jsx,ts,tsx}"],
-    ignores: ["node_modules/", ".next/", "dist/"],
-    rules: {},
-  },
-];
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
+
+export default eslintConfig;
