60b2cc3657518ed844d4f3bde9edb063a496eedb
biyash-1 <sthaalish39@gmail.com>
2025-12-06 17:22:02 +0545

    type fix of eslins

--- eslint.config.js
+++ eslint.config.js
@@ -1,43 +1,44 @@
-// eslint.config.js
+// eslint.config.js (ESM)
 import js from "@eslint/js";
 import { defineFlatConfig } from "eslint-define-config";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
 
 export default defineFlatConfig([
   {
-    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"],
+    ignores: ["node_modules/**", ".next/**", "out/**", "build/**"],
   },
 
+  // Base JS recommended
   js.configs.recommended,
 
-  // TS recommended config (ESM)
+  // TypeScript plugin + parser WITHOUT extends
   {
-    ...tsPlugin.configs.recommended,
-  },
-
-  {
+    files: ["**/*.ts", "**/*.tsx"],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
       parser: tsParser,
       parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
         ecmaFeatures: { jsx: true },
       },
     },
-
     plugins: {
       "@typescript-eslint": tsPlugin,
     },
-
-    settings: {
-      react: {
-        version: "detect",
-      },
+    rules: {
+      ...tsPlugin.configs["recommended-type-checked"].rules, // safe for flat config
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
     },
+  },
 
+  // React rules (manual, because eslint-config-next cannot be used)
+  {
+    files: ["**/*.tsx"],
     rules: {
       "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
     },
   },
 ]);
